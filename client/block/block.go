84792c212651368f8fab056bbdd18a6cf47fd81d
NPC-Chain <charleyvitc56@gmail.com>
2021-06-30 10:04:07 +0800

    Define block structure

--- client/block/block.go
+++ client/block/block.go
@@ -1,7 +1,11 @@
 package block
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"strconv"
+	"time"
 
 	"github.com/NPC-Chain/npccbase/client/context"
 	"github.com/NPC-Chain/npccbase/client/types"
@@ -11,6 +15,14 @@ import (
 	go_amino "github.com/tendermint/go-amino"
 )
 
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
+}
 func blockCommand(cdc *go_amino.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "block [height]",
@@ -46,4 +58,46 @@ func blockCommand(cdc *go_amino.Codec) *cobra.Command {
 	viper.BindPFlag(types.FlagNode, cmd.Flags().Lookup(types.FlagNode))
 	cmd.Flags().Bool(types.FlagJSONIndet, false, "print indent result json")
 	return cmd
+}
+// NewBlock creates and returns Block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+// Serialize serializes the block
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+// DeserializeBlock deserializes a block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
